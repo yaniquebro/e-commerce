a8394423ed834e58e51fc62fdcb561cc34b1abf9
Yanique Brooks <yaniquebro@gmail.com>
2024-07-24 20:52:29 -0400

    models added

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -1,12 +1,35 @@
 const { Model, DataTypes } = require('sequelize');
 
 const sequelize = require('../config/connection');
+const Product = require('./Product');
+const Tag = require('./Tag');
 
 class ProductTag extends Model {}
 
 ProductTag.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    product_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "product",
+        key: 'id'
+      }
+    },
+    tag_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: "tag",
+        key: 'id'
+      }
+    }
   },
   {
     sequelize,
